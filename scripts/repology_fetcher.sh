e9b724c98ca7cb446dbf0836cdcff8b251a0063c
Azathothas <AjamX101@gmail.com>
2024-11-23 16:03:46 +0545

    generate proper `distro_pkg` & `homepage`

--- scripts/repology_fetcher.sh
+++ scripts/repology_fetcher.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 #
+# REQUIRES: awk + coreutils + curl + grep + jq + sed + yq
 # source <(curl -qfsSL "https://raw.githubusercontent.com/pkgforge/soarpkgs/refs/heads/main/scripts/repology_fetcher.sh")
 # source <(curl -qfsSL "https://l.ajam.dev/repology-fetcher")
 #set -x
@@ -23,9 +24,44 @@ repology_fetcher()
    curl -A "${USER_AGENT}" -qfsSL "https://repology.org/api/v1/project/${REPOLOGY_PKG}" -o "${TMP_JSON}" || curl -qfsSL "https://api.rl.pkgforge.dev/api/v1/project/${REPOLOGY_PKG}" -o "${TMP_JSON}"
    if [[ -f "${TMP_JSON}" ]] && [[ $(stat -c%s "${TMP_JSON}") -gt 100 ]]; then
      #Description
-      jq -r '.[] | select(.summary != null and .summary != "") | .summary' "${TMP_JSON}" | sed -e 's/["'\''`|]//g' -e 's/^[ \t]*//;s/[ \t]*$//' | sort -u | grep -viE 'l10n|ICU data|language pack' | awk '{print "description: \"" $0 "\""}' ; echo
+      jq -r '.[] | select(.summary != null and .summary != "") | .summary' "${TMP_JSON}" | sed -e 's/["'\''`|]//g' -e 's/^[ \t]*//;s/[ \t]*$//' | sort -u | grep -viE 'l10n|ICU data|language pack' | awk '{print "description: \"" $0 "\""}' | awk '{ print length(), $0 }' | sort -n | awk '{sub(/^[0-9]+ /,""); print}' ; echo
      #distro_pkg
-      jq -r '.[] | "[\(.repo)/\(.subrepo // "")] --> \(.srcname)"' "${TMP_JSON}" | sed -e 's/["'\''`|]//g' -e 's/^[ \t]*//;s/[ \t]*$//' | sort -u | grep -iE 'alpine_edge|arch|aur|debian_12|debian_13|debian_unstable|nix_unstable' ; echo
+      #jq -r '.[] | "[\(.repo)/\(.subrepo // "")] --> \(.srcname)"' "${TMP_JSON}" | sed -e 's/["'\''`|]//g' -e 's/^[ \t]*//;s/[ \t]*$//' | sort -u | grep -iE 'alpine_edge|arch|aur|debian_12|debian_13|debian_unstable|nix_unstable' ; echo
+      jq -r '{
+        distro_pkg: (
+          {
+            archlinux: {
+              aur: (
+                [ .[] | select(.repo | test("aur")) ]
+                | map(.srcname) | unique
+              ),
+              extra: (
+                [ .[] | select(.repo == "arch" and .subrepo == "extra") ]
+                | map(.srcname) | unique
+              )
+            },
+            alpine: (
+              [ .[] | select(.repo | test("^alpine_")) ]
+              | map(.srcname) | unique
+            ),
+            debian: (
+              [ .[] | select(.repo | test("^debian_")) ]
+              | map(.srcname) | unique
+            ),
+            nixpkgs: (
+              [ .[] | select(.repo | test("^nix_")) ]
+              | map(.srcname) | unique
+            )
+          }
+        )
+      }' "${TMP_JSON}" | yq eval 'sort_keys(.distro_pkg) | del(.. | select(tag == "!!seq" and length == 0))' -P -oyaml | sed 's/- \(.*\)/- "\1"/' ; echo
+     #HomePage
+      {
+       #Requires Xq 
+        #curl -A "${USER_AGENT}" -qfsSL "https://repology.org/project/${REPOLOGY_PKG}/information" | xq --xpath="//section[@id='Homepage_links']//a[not(contains(@href, '/link/'))]/@href" | grep -oP 'http[^\s]+' | sed 's/\/$//' | awk 'BEGIN {print "homepage:"} {print "  - \"" $1 "\""}' | yq 'del(.. | select(tag == "!!seq" and length == 0))' -P -oyaml | sed 's/- \(.*\)/- "\1"/' ; echo
+       #Just sed 
+        curl -A "${USER_AGENT}" -qfsSL "https://repology.org/project/${REPOLOGY_PKG}/information" | sed -n '/<section id="Homepage_links">/,/<\/section>/p' | sed -n 's/.*<a href="\([^"]*\)".*/\1/p' | grep -oP 'http[^\s]+' | sed 's/\/$//' | awk 'BEGIN {print "homepage:"} {print "  - \"" $1 "\""}' | yq 'del(.. | select(tag == "!!seq" and length == 0))' -P -oyaml | sed 's/- \(.*\)/- "\1"/' ; echo
+      } 2>/dev/null
      #license
       jq -r '.[] | select(.licenses != null and .licenses != "") | .licenses[]' "${TMP_JSON}" | sed -e 's/["'\''`|]//g' -e 's/^[ \t]*//;s/[ \t]*$//' -e 's/(.*)//g' | sort -u | awk '{print "  - \"" $0 "\""}' | awk 'BEGIN {print "license:"} {print}' ; echo
      #tag
